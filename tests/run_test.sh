b1954279dc54b48bb786a48fc6f2fee4fb30f35b
Michael Kotliar <misha.kotliar@gmail.com>
2018-04-19 17:16:26 -0400

    In develompent

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 mkdir ./single_chromosome/outputs
-bcftools query -f '%CHROM\t%POS0\t%REF\t%ALT\n' -i 'TYPE!="snp"' ./single_chromosome/inputs/indels.vcf | awk 'BEGIN {chr=""} {if (chr!=$1){chr=$1; balance=0; print $1"\t"0"\t"0}; diff=length($4)-length($3); if (diff<0) print $1"\t"$2+1+balance"\t"balance+diff; else for(i=1;i<=diff;i++){print $1"\t"$2+1+i+balance"\t"balance+i}; balance=balance+diff}' | sort -k 1,1 -k 2,2n > ./single_chromosome/outputs/chr_pos0_ref_alt.tsv
+#bcftools query -f '%CHROM\t%POS0\t%REF\t%ALT\n' -i 'TYPE!="snp"' ./single_chromosome/inputs/indels.vcf | awk 'BEGIN {chr=""} {if (chr!=$1){chr=$1; balance=0; print $1"\t"0"\t"0}; diff=length($4)-length($3); if (diff<0) print $1"\t"$2+1+balance"\t"balance+diff; else for(i=1;i<=diff;i++){print $1"\t"$2+1+i+balance"\t"balance+i}; balance=balance+diff}' | sort -k 1,1 -k 2,2n > ./single_chromosome/outputs/chr_pos0_ref_alt.tsv
+#bcftools query -f '%CHROM\t%POS0\t%REF\t%ALT\n' -i 'TYPE!="snp"' ./single_chromosome/inputs/indels.vcf  | awk 'BEGIN {chr=""} {if (chr!=$1){chr=$1; balance=0; current_pos=0; previous_pos=0; print $1"\t"0"\t"0;}; diff=length($4)-length($3); current_pos=$2+1+balance; if (diff<0) print $1"\t"current_pos"\t"balance+diff; else for(i=1;i<=diff;i++){print $1"\t"current_pos+i"\t"balance+i}; balance=balance+diff; previous_pos=current_pos}' | sort -k 1,1 -k 2,2n > ./single_chromosome/outputs/chr_pos0_ref_alt.tsv
+bcftools query -f '%CHROM\t%POS0\t%REF\t%ALT\n' ./single_chromosome/inputs/indels.vcf  | awk 'BEGIN {chr=""} {if (chr!=$1){chr=$1; balance=0; previous_pos=-1; print $1"\t"0"\t"0;}; if ($2<=previous_pos) next; diff=length($4)-length($3); current_pos=$2+1+balance; if (diff<0) print $1"\t"current_pos"\t"balance+diff; else for(i=1;i<=diff;i++){print $1"\t"current_pos+i"\t"balance+i}; balance=balance+diff; previous_pos=$2+length($3)-1}' | sort -k 1,1 -k 2,2n > ./single_chromosome/outputs/chr_pos0_ref_alt.tsv
 python ../allele_project/main.py -r ./single_chromosome/outputs/chr_pos0_ref_alt.tsv -b ./single_chromosome/inputs/input_reads.bedGraph -o ./single_chromosome/outputs/output_reads.bedGraph
 diff ./single_chromosome/outputs/output_reads.bedGraph ./single_chromosome/controls/control_reads.bedGraph
 
-python ../allele_project/main.py -r ./single_chromosome/outputs/chr_pos0_ref_alt.tsv -b ./single_chromosome/inputs/input_coverage.bedGraph -o ./single_chromosome/outputs/output_coverage.bedGraph
-diff ./single_chromosome/outputs/output_coverage.bedGraph ./single_chromosome/controls/control_coverage.bedGraph
+#python ../allele_project/main.py -r ./single_chromosome/outputs/chr_pos0_ref_alt.tsv -b ./single_chromosome/inputs/input_coverage.bedGraph -o ./single_chromosome/outputs/output_coverage.bedGraph
+#diff ./single_chromosome/outputs/output_coverage.bedGraph ./single_chromosome/controls/control_coverage.bedGraph
 
 #rm -rf ./single_chromosome/outputs
